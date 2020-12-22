a0194eb11660559a1ff29856856f8f6086a9259f
Paul <webdevpaulmason@gmail.com>
2020-12-22 11:57:57 -0800

    Testing layout

--- src/App.js
+++ src/App.js
@@ -9,19 +9,59 @@ import RightBar from "./components/RightBar/RightBar";
 
 function App() {
   return (
-    <div className="App">
-      <div className="Home">
-        <LeftBar />
-        <section className="main">
-          <Hero />
-          <div className="App__portfolio u-margin-top-small u-margin-center">
-            <PortfolioFilter />
-            <PortfolioDetails />
+    // <div className="App">
+    //   <div className="Home">
+    //     <LeftBar />
+    //     <section className="main">
+    //       <Hero />
+    //       <div className="App__portfolio u-margin-top-small u-margin-center">
+    //         <PortfolioFilter />
+    //         <PortfolioDetails />
+    //       </div>
+    //     </section>
+    //     <RightBar />
+    //   </div>
+    //   <Contact />
+    // </div>
+    <div className="Portfolio">
+      <div className="Content">
+        <div className="Header">
+          <div className="Header__hero">Paul Mason</div>
+          <div className="Header__resume">My Resume</div>
+        </div>
+        <div className="Main">
+          <div className="Main__dynamic">
+            <div className="Main__dynamic__default">
+              <div className="Main__dynamic__default--about-me">
+                <p>
+                  Passionate web developer and life-long learner with a love for
+                  developing applications for genuine user needs. I get excited
+                  about creating new applications that tap into the power of
+                  public APIs. With the careful use of asynchronous javascript
+                  and by creating responsive front-end interfaces, I am able to
+                  consisely display the most important pieces of information.
+                </p>
+                <p>
+                  I have experience building full stack applications with both
+                  React and Vue on the front end. MongoDB is my back-end of
+                  choice. I am always looking to learn new technologies and
+                  implement creative solutions to the problems I encounter. Feel
+                  free to look at my portfolio below. I've tried to make it all
+                  fit on one page, because I know your time is valuable.
+                </p>
+              </div>
+              <div className="Main__dynamic__default--my-skills">My skills</div>
+            </div>
+            {/* <div className="Main__dynamic__projects">
+            <div className="Main__dynamic__projects--details">
+              Project Details
+            </div>
+          </div> */}
           </div>
-        </section>
-        <RightBar />
+          <div className="Main__project-selector">Select a project</div>
+        </div>
       </div>
-      <Contact />
+      <div className="Footer">FOOTER</div>
     </div>
   );
 }
